37bc3aa4c6833cbb138a22bfed7ddedf93da11e1
Vid Luther <vid@luther.io>
2026-09-15 06:50:13 +0530

    fix(cask): drop deprecated verified url parameter

--- Casks/spatie-bloom.rb
+++ Casks/spatie-bloom.rb
@@ -2,8 +2,7 @@ cask "spatie-bloom" do
   version "1.18.0"
   sha256 "00568bfd93d8879b4c11e925fe512d5d6286e533c7b3efcc45f6882fb7cd0ab6"
 
-  url "https://github.com/spatie/bloom/releases/download/v#{version}/Bloom-#{version}.dmg",
-      verified: "github.com/spatie/bloom/"
+  url "https://github.com/spatie/bloom/releases/download/v#{version}/Bloom-#{version}.dmg"
   name "Bloom"
   desc "Agent development environment"
   homepage "https://runbloom.app/"
